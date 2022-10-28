e42ab3af885687434af2c4ad111e0c89d6317122
Tom <tomjw@protonmail.ch>
2022-10-28 18:44:29 +0100

    Update tif.rs

--- src/tif.rs
+++ src/tif.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
 use std::io::{BufWriter, Write};
+use std::mem::size_of;
 
 use image::GenericImage;
 use texpresso::Format;
+use texpresso::Algorithm;
 
 pub const SUPPORTED_FORMATS: [&str;8] = [ 
     "argb8888",  
@@ -15,48 +17,43 @@ pub const SUPPORTED_FORMATS: [&str;8] = [
     "dxt5" 
 ];
 
-pub fn write_tif_file_ma1(buf: &BufWriter<File>, img: &image::DynamicImage, format: &str) {
-    let (width, height) = img.dimensions();
+pub fn write_tif_file_ma1(buf: &mut BufWriter<File>, img: &image::DynamicImage, format: &str) {
+    let (width , height) = img.dimensions();
 
-    let mut flags: u32 = 0;
+    let flags: u32 = match format {
+        "argb8888"  => 0x00000000,
+        "rgb565"    => 0x00000001,
+        "argb4444"  => 0x00000003,
+        "l8"        => 0x00000005,
+        "la88"      => 0x00000007,
+        "dxt1"      => 0x00000100,
+        "dxt3"      => 0x00000300,
+        "dxt5"      => 0x00000500,
+        _           => panic!("Unrecognised format")
+    };
 
-    let dxt_fmt: texpresso::Format;
-    let mut is_dxt = false;
+    if format == "dxt1" || format == "dxt3" || format == "dxt5" {
+        let dxt_fmt: texpresso::Format = match format {
+            "dxt1"  => Format::Bc1,
+            "dxt3"  => Format::Bc3,
+            "dxt5"  => Format::Bc5,
+            _       => panic!("Unrecognised DXT format")
+        };
 
-    if format == "argb8888" {
+        assert!(width % 2 == 0 && height & 2 == 0, "Source image dimensions must be powers of 2 when using dxt1, dxt3, or dxt5");
 
-    }
-    else if format == "rgb565" {
-        flags |= 0x00000001;
-    }
-    else if format == "argb4444" {
-        flags |= 0x00000003;
-    }
-    else if format == "dxt1" {
-        assert!(width % 2 == 0 && height % 2 == 0);
-        flags |= 0x00000100;
-        dxt_fmt = Format::Bc1;
-        is_dxt = true;
-    } 
-    else if format == "dxt3" {
-        assert!(width % 2 == 0 && height % 2 == 0);
-        flags |= 0x00000300;
-        dxt_fmt = Format::Bc3;
-        is_dxt = true;
-    }
-    else if format == "dxt5" {
-        assert!(width % 2 == 0 && height % 2 == 0);
-        flags |= 0x00000500;
-        dxt_fmt = Format::Bc5;
-        is_dxt = true;
-    }
+        let params = texpresso::Params { algorithm: Algorithm::ClusterFit, weights: texpresso::COLOUR_WEIGHTS_PERCEPTUAL, weigh_colour_by_alpha: false };
+        let mut dest_pixels = vec![0u8; dxt_fmt.compressed_size(width as usize, height as usize)];
 
-    if is_dxt {
+        dxt_fmt.compress(&img.raw_pixels(), width as usize, height as usize, params, &mut dest_pixels);
 
+        buf.write(&dest_pixels);
     }
     else {
-
+        
     }
+
+
 }
 
 fn write_header_ma1(buf: &mut BufWriter<File>, flags: u32, width: u32, height: u32, mips: u32, size: u32) {
@@ -111,4 +108,67 @@ fn write_header_ma1(buf: &mut BufWriter<File>, flags: u32, width: u32, height: u
     // Write "bits"
     buf.write(b"bits");
     buf.write(&bits.to_be_bytes());
+}
+
+fn get_bytes_per_pixel(fmt: &str) -> usize {
+    match fmt {
+        "argb8888"  => size_of::<u32>(),
+        "rgb565"    => size_of::<u16>(),
+        "argb4444"  => size_of::<u16>(),
+        "l8"        => size_of::<u8>(),
+        "la88"      => size_of::<u16>(),
+        _           => size_of::<u32>()
+    }
+}
+
+fn pad_width(width: u32) -> u32 {
+    if width % 16 == 0 { width } else { width + (16 - width % 16) }
+}
+
+fn pixel_to_argb8888(src_pixel: image::Rgba<u8>) -> u32 {
+    let r = src_pixel.data[0];
+    let g = src_pixel.data[1];
+    let b = src_pixel.data[2];
+    let a = src_pixel.data[3];
+
+    (a << 24) as u32 |
+    (r << 16) as u32 |
+    (g << 8) as u32 |
+    b as u32
+}
+
+fn pixel_to_argb4444(src_pixel: image::Rgba<u8>) -> u16 {
+    let r = src_pixel.data[0];
+    let g = src_pixel.data[1];
+    let b = src_pixel.data[2];
+    let a = src_pixel.data[3];
+
+    ((a >> 4) << 12) as u16 |
+    ((r >> 4) << 8) as u16 |
+    ((g >> 4) << 4) as u16 |
+    (b >> 4) as u16
+}
+
+fn pixel_to_rgb565(src_pixel: image::Rgba<u8>) -> u16 {
+    let r = src_pixel.data[0];
+    let g = src_pixel.data[1];
+    let b = src_pixel.data[2];
+    let a = src_pixel.data[3];
+
+   // ((a >> 7) << 12) as u16 |
+    ((r >> 3) << 11) as u16 |
+    ((g >> 2) << 6) as u16 |
+    (b >> 3) as u16
+}
+
+fn pixel_to_l8(src_pixel: image::Luma<u8>) -> u8 {
+    src_pixel.data[0]
+}
+
+fn pixel_to_la88(src_pixel: image::LumaA<u8>) -> u16 {
+    let l = src_pixel.data[0];
+    let a = src_pixel.data[1];
+
+    (l << 8) as u16 |
+    a as u16
 }
